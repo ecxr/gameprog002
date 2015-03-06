75b4d3d91753b4d80a83255d48f26703967b9a47
Sky King <skyking@gmail.com>
2015-03-06 14:52:07 -0800

    Using the deck class

--- examples/week2/UsingDeckClass/Program.cs
+++ examples/week2/UsingDeckClass/Program.cs
@@ -16,6 +16,30 @@ namespace UsingDeckClass
         /// <param name="args">command-line args</param>
         static void Main(string[] args)
         {
+            Deck deck = new Deck();
+
+            // print deck empty information
+            Console.WriteLine("Empty: " + deck.Empty);
+
+            //deck.Print();
+
+            // tell the deck to shuffle itself
+            //deck.Shuffle();
+
+            // cut the deck
+            //deck.Cut(26);
+
+            // take top card and print info
+            deck.TakeTopCard();
+            //Card card = deck.TakeTopCard();
+            //Console.WriteLine(card.Rank + " of " + card.Suit);
+
+            //Console.WriteLine();
+            //deck.Print();
+
+
+
+            Console.WriteLine();
         }
     }
 }
