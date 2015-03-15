8baf83970dd9b4be8b2ab01c2f4413972a80eb9d
Sky King <skyking@gmail.com>
2015-03-15 11:53:58 -0700

    Week 3 examples

---
+++ examples/week3/61_IntroToStrings/IntroToStrings/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntroToStrings
+{
+    /// <summary>
+    /// Demonstrates string basics
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates how to find a substring and extract from a larger string
+        /// </summary>
+        static void ReadCSV() 
+        {
+            // read in csv string
+            Console.Write("Enter name and percent (name,percent): ");
+            string csvstring = Console.ReadLine();
+            
+            // find comma location
+            int commaLocation = csvstring.IndexOf(',');
+
+            // extract name and percent
+            string name = csvstring.Substring(0, commaLocation);
+            float percent = float.Parse(csvstring.Substring(commaLocation + 1));
+
+            // print name and percent
+            Console.WriteLine(name);
+            Console.WriteLine(percent);
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Demonstrates string basics
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            ReadCSV();
+
+            // prompt for and read in gamertag
+            Console.Write("Enter gamertag: ");
+            string gamertag = Console.ReadLine();
+
+            // prompt for and read in level
+            Console.Write("Enter level: ");
+            int level = int.Parse(Console.ReadLine());
+
+            // extract the first character of the gamertag
+            char firstGamertagCharacter = gamertag[0];
+
+            // print out values
+            Console.WriteLine("Gamertag: " + gamertag);
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("First gamertag character: " + firstGamertagCharacter);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ examples/week3/71_SwitchStatements/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwitchStatements
+{
+    /// <summary>
+    /// Demonstrates the switch statement
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates the switch statement
+        /// </summary>
+        static void TestSwitch()
+        {
+            // ask for and get user answer
+            Console.Write("Pick up the shiny thing? (y, n): ");
+            char answer = char.ToLower(Console.ReadLine()[0]);
+
+            // print appropriate message
+            switch (answer)
+            {
+                case 'y':
+                    Console.WriteLine("You have the shiny object");
+                    break;
+
+                case 'n':
+                    Console.WriteLine("You don't have the shiny object");
+                    break;
+                
+                default:
+                    Console.WriteLine("Please choose a valid response");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Demonstrates the if statement
+        /// </summary>
+        static void TestIf()
+        {
+            // ask for and get user answer
+            Console.Write("Pick up the shiny thing? (y, n): ");
+            char answer = char.ToLower(Console.ReadLine()[0]);
+
+            // print appropriate message
+            if (answer == 'y')
+            {
+                Console.WriteLine("You have the shiny object");
+            }
+            else if (answer == 'n')
+            {
+                Console.WriteLine("You don't have the shiny object");
+            }
+            else
+            {
+                Console.WriteLine("Please choose a valid response");
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Demonstrates the if and switch statements
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            TestIf();
+            TestSwitch();
+        }
+    }
+}
