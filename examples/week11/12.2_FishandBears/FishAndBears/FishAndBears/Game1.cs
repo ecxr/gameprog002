afbc49f9477efb20031441be9c7e5c54a53f9d2f
Sky King <skyking@gmail.com>
2015-05-10 12:40:25 -0700

    Week11 examples - audio, keyboard, text output

---
+++ examples/week11/12.2_FishandBears/FishAndBears/FishAndBears/Game1.cs
@@ -0,0 +1,284 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace FishAndBears
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        const int WINDOW_WIDTH = 584;
+        const int WINDOW_HEIGHT = 438;
+
+        // menu objects
+        Menu mainMenu;
+
+        // game objects
+        Fish fish;
+        List<TeddyBear> bears = new List<TeddyBear>();
+
+        // field to keep track of game state
+        static GameState state;
+
+        // audio components
+        AudioEngine audioEngine;
+        WaveBank waveBank;
+        SoundBank soundBank;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // set resolution and make mouse visible
+            graphics.PreferredBackBufferWidth = WINDOW_WIDTH;
+            graphics.PreferredBackBufferHeight = WINDOW_HEIGHT;
+            IsMouseVisible = true;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // load audio content
+            audioEngine = new AudioEngine(@"Content\GameAudio.xgs");
+            waveBank = new WaveBank(audioEngine, @"Content\Wave Bank.xwb");
+            soundBank = new SoundBank(audioEngine, @"Content\Sound Bank.xsb");
+
+            // start playing background music
+            soundBank.PlayCue("backgroundMusic");
+
+            // initialize menu objects
+            mainMenu = new Menu(Content, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+            // initialize fish
+            fish = new Fish(Content, "fish", WINDOW_WIDTH / 6, WINDOW_WIDTH / 6,
+                WINDOW_WIDTH, WINDOW_HEIGHT);
+
+            // initialize bear list
+            string baseSpriteName = "teddybear";
+            Random rand = new Random();
+            for (int x = 150; x <= 600; x += 150)
+            {
+                for (int y = 150; y <= 450; y += 150)
+                {
+                    bears.Add(new TeddyBear(Content, baseSpriteName + rand.Next(3), 
+                        x, y, WINDOW_WIDTH, WINDOW_HEIGHT));
+                }
+            }
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            // process input based on game state
+            if (state == GameState.MainMenu)
+            {
+                // update main menu
+                mainMenu.Update(Mouse.GetState(), soundBank);
+            }
+            else if (state == GameState.Play)
+            {
+                // update fish
+                fish.Update(Keyboard.GetState());
+
+                // update teddy bears
+                foreach (TeddyBear bear in bears)
+                {
+                    bear.Update();
+                }
+
+                // check collisions between fish and teddy bears
+                CheckAndResolveFishBearCollisions();
+
+                // check and resolve collisions between teddy bears
+                CheckAndResolveBearCollisions();
+
+                // check for all teddy bears dead
+                if (bears.Count == 0)
+                {
+                    ChangeState(GameState.Quit);
+                }
+            }
+            else
+            {
+                this.Exit();
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // draw based on game state
+            spriteBatch.Begin();
+            if (state == GameState.MainMenu)
+            {
+                // draw the main menu
+                mainMenu.Draw(spriteBatch);
+            }
+            else if (state == GameState.Play)
+            {
+                // draw the game objects
+                fish.Draw(spriteBatch);
+                foreach (TeddyBear bear in bears)
+                {
+                    bear.Draw(spriteBatch);
+                }
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        /// <summary>
+        /// Changes the state of the game
+        /// </summary>
+        /// <param name="newState">the new game state</param>
+        public static void ChangeState(GameState newState)
+        {
+            state = newState;
+        }
+
+        /// <summary>
+        /// Checks and resolves collisions between teddy bears in the game
+        /// </summary>
+        private void CheckAndResolveBearCollisions()
+        {
+            for (int i = 0; i < bears.Count; i++)
+            {
+                for (int j = i + 1; j < bears.Count; j++)
+                {
+                    if (bears[i].Active && bears[j].Active &&
+                        bears[i].CollisionRectangle.Intersects(
+                            bears[j].CollisionRectangle))
+                    {
+                        bears[i].Bounce();
+                        bears[j].Bounce();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks and resolves collisions between the fish and the teddy bears in the game
+        /// </summary>
+        private void CheckAndResolveFishBearCollisions()
+        {
+            // check collisions between fish and teddy bears
+            for (int i = bears.Count - 1; i >= 0; i--)
+            {
+                if (fish.Active && bears[i].Active &&
+                    fish.CollisionRectangle.Intersects(bears[i].CollisionRectangle))
+                {
+                    // check where the collision occurred
+                    Rectangle overlap = Rectangle.Intersect(fish.CollisionRectangle,
+                        bears[i].CollisionRectangle);
+                    if (overlap.Left <= fish.Front && overlap.Right >= fish.Front)
+                    {
+                        // fish ate the bear
+                        bears.RemoveAt(i);
+
+                        // play eating sound
+                        soundBank.PlayCue("destroy");
+                    }
+                    else
+                    {
+                        // non-head collision
+                        bears[i].Bounce();
+
+                        // play bouncing sound
+                        soundBank.PlayCue("bounce");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes the dead teddy bears from the list
+        /// </summary>
+        private void RemoveDeadTeddyBears()
+        {
+            for (int i = bears.Count - 1; i >= 0; i--)
+            {
+                if (!bears[i].Active)
+                {
+                    bears.RemoveAt(i);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if all the bears are dead
+        /// </summary>
+        private void CheckBearsAllDead()
+        {
+            bool allBearsDead = true;
+            foreach (TeddyBear bear in bears)
+            {
+                if (bear.Active)
+                {
+                    allBearsDead = false;
+                    break;
+                }
+            }
+            if (allBearsDead)
+            {
+                ChangeState(GameState.Quit);
+            }
+        }
+    }
+}
